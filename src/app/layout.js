ff3554ca36fd791aef85773d1e2b5f3bd925efd1
nexovisualss <nexovisualss@gmail.com>
2026-07-28 12:31:20 +0530

    add site.webmanifest

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,22 +27,13 @@ export const metadata = {
   title: "Kalloviyam",
   description: "Construction Company Website",
 
+  manifest: "/site.webmanifest",
+
   icons: {
     icon: [
-      {
-        url: "/favicon.ico",
-        sizes: "any",
-      },
-      {
-        url: "/favicon-32x32.png",
-        type: "image/png",
-        sizes: "32x32",
-      },
-      {
-        url: "/favicon-16x16.png",
-        type: "image/png",
-        sizes: "16x16",
-      },
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
     apple: "/apple-touch-icon.png",
   },
